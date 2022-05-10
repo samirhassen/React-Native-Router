606e07d82a2576d6798ce4309704ce8f5ea396e6
samirhassen <hassen.samir@gmail.com>
2022-05-10 13:22:22 +0200

    'initial'

--- App.js
+++ App.js
@@ -1,12 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { 
+  StyleSheet, 
+  Text, 
+  View,
+  Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+//import Home from './Home'
+
+const Stack = createNativeStackNavigator();
+
+function Home({navigation}) {
+  return (
+    <View style={styles.container}>
+      <Text>Home Sweet Home</Text>
+      <StatusBar style="auto" />
+      <Button
+        title='Go to login'
+        onPress={() => navigation.navigate('Login', {data : "Jensen is here"})}
+      />
+    </View>
+  );
+}
+
+function Login({navigation, route}) {
+  return (
+    <View style={styles.container}>
+      <Text>Login Screen - {route.params.data}</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name='Home' component={Home} />
+        <Stack.Screen name='Login' component={Login} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
